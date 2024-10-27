7447dae4de85d2f636c6420fa8c3513a561f2250
Ellis velandia <eyis619@gmail.com>
2024-10-27 16:52:00 -0500

    data types

--- main.java
+++ main.java
@@ -1,6 +1,26 @@
 class ClassName {
     public static void main(String arg[]) {
+        // Data types
+        int var = 10; // number , 4 byte
+        byte bt = 2; // number , 1 byte
+        short st = 34; // nu,mber , 2 byte
+        long lg = 3333333333L; // number , 8 byte
+
+        char ch = 'A'; // character , 2 byte
+        boolean bool = true; // boolean , 1 byte
+        float ft = 10.45f; // number , 4 byte
+        double db = 34.34; // number , 8 byte
+
+        // multiple variables
+        int a = 10, b = 20, c = 30;
+        int x; // variable declaration
+        int y, z; // variable declaration
+        x = 10; // variable initialization
+        y = 20; // variable initialization
+        z = 30; // variable initialization
         System.out.println("Hello World");
+        System.out.println(x + y + z);
+        System.out.println("The value of y is " + y);
     }
 }
 
