36ea513f189d71699c24d4d40ae557c3bd0036da
Ellis velandia <eyis619@gmail.com>
2024-10-27 16:26:23 -0500

    start

---
+++ main.java
@@ -0,0 +1,12 @@
+class ClassName {
+    public static void main(String arg[]) {
+        System.out.println("Hello World");
+    }
+}
+
+// Java execution
+
+// 1. Write code
+// 2 . Compile Code (JKD Compiler read codes. Chekss Error,)
+// 3. Compiler Generates Byte Code file with same name as class name
+// 4.Byte Code is executed by JVM(Java Virtual Machine)
