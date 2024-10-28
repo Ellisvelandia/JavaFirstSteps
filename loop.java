6b4e32c9d6a45a41874c634f0d62f88355363737
Ellis velandia <eyis619@gmail.com>
2024-10-27 19:48:59 -0500

    task loops

---
+++ loop.java
@@ -0,0 +1,14 @@
+public class loop {
+    public static void main(String[] args) {
+
+        // Task create a program to print table of any number
+
+        int table = 5;
+
+        for (int num = 1; num <= 10; num++) {
+            System.out.println(table + " X " + num + "=" + table * num);
+        }
+
+        System.out.println("Hello World");
+    }
+}
